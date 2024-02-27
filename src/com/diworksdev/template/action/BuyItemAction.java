5c53deefc7e283a77704bc87c58dd2e94b80ee20
ibukiyabu <ibuki.y2115@gmail.com>
2024-02-27 20:59:32 +0900

    クウォーテーションが抜けてた（修正）

--- src/com/diworksdev/template/action/BuyItemAction.java
+++ src/com/diworksdev/template/action/BuyItemAction.java
@@ -25,10 +25,10 @@ public class BuyItemAction extends ActionSupport implements SessionAware {
 		
 		if(pay.equals("1")) {
 			payment = "現金払い";
-			session.put(pay, payment);
+			session.put("pay", payment);
 		} else {
 			payment = "クレジットカード";
-			session.put(pay, payment);
+			session.put("pay", payment);
 		}
 		return result;
 	}
